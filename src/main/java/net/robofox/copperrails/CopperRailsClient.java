6882e1927305982ce9027d7238624a737126c79c
FXCourel <fxcourel@orange.fr>
2024-08-12 16:41:36 +0200

    3D builtin Texture pack

--- src/main/java/net/robofox/copperrails/CopperRailsClient.java
+++ src/main/java/net/robofox/copperrails/CopperRailsClient.java
@@ -2,14 +2,26 @@ package net.robofox.copperrails;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.robofox.copperrails.block.ModBlocks;
 
 public class CopperRailsClient implements ClientModInitializer {
 
     private static void cutout(Block block) {
         BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
+    }
+
+    private static void initializeResourcePack() {
+        Identifier id = Identifier.of(CopperRails.MOD_ID, "copperrails3d");
+        ModContainer modContainer = FabricLoader.getInstance().getModContainer(CopperRails.MOD_ID).orElseThrow();
+        ResourceManagerHelper.registerBuiltinResourcePack(id, modContainer, Text.of("CopperRails 3D Rails"), ResourcePackActivationType.NORMAL);
     }
 
     @Override
@@ -23,5 +35,6 @@ public class CopperRailsClient implements ClientModInitializer {
         cutout(ModBlocks.WAXED_WEATHERED_COPPER_RAIL);
         cutout(ModBlocks.WAXED_OXIDIZED_COPPER_RAIL);
         cutout(ModBlocks.RAIL_CROSSING);
+        initializeResourcePack();
     }
 }
