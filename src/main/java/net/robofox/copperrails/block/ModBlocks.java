0755479f3436c236db88ae96b835a04b7c09cf2a
BigBeefyCow666 <156566452+BigBeefyCow666@users.noreply.github.com>
2025-04-21 14:41:19 +1000

    Update ModBlocks.java

--- src/main/java/net/robofox/copperrails/block/ModBlocks.java
+++ src/main/java/net/robofox/copperrails/block/ModBlocks.java
@@ -2,6 +2,8 @@ package net.robofox.copperrails.block;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.OxidizableBlocksRegistry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -20,15 +22,17 @@ import net.robofox.copperrails.block.custom.OxidizableCopperRailBlock;
 
 public class ModBlocks {
 
-    public static final Block COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.UNAFFECTED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.COLOR_ORANGE)), "copper_rail");
-    public static final Block EXPOSED_COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.EXPOSED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.TERRACOTTA_LIGHT_GRAY)), "exposed_copper_rail");
-    public static final Block WEATHERED_COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.WEATHERED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.WARPED_STEM)), "weathered_copper_rail");
-    public static final Block OXIDIZED_COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.OXIDIZED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.WARPED_NYLIUM)), "oxidized_copper_rail");
-    public static final Block WAXED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.COPPER_RAIL)), "waxed_copper_rail");
-    public static final Block WAXED_EXPOSED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.EXPOSED_COPPER_RAIL)), "waxed_exposed_copper_rail");
-    public static final Block WAXED_WEATHERED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.WEATHERED_COPPER_RAIL)), "waxed_weathered_copper_rail");
-    public static final Block WAXED_OXIDIZED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.OXIDIZED_COPPER_RAIL)), "waxed_oxidized_copper_rail");
-    public static final Block RAIL_CROSSING = register(new CrossingRailBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL)), "rail_crossing");
+//    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "waxed_oxidized_copper_rail")))
+
+    public static final Block COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.UNAFFECTED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.COLOR_ORANGE).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "copper_rail")))), "copper_rail");
+    public static final Block EXPOSED_COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.EXPOSED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.TERRACOTTA_LIGHT_GRAY).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "exposed_copper_rail")))), "exposed_copper_rail");
+    public static final Block WEATHERED_COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.WEATHERED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.WARPED_STEM).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "weathered_copper_rail")))), "weathered_copper_rail");
+    public static final Block OXIDIZED_COPPER_RAIL = register(new OxidizableCopperRailBlock(WeatheringCopper.WeatherState.OXIDIZED, BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).mapColor(MapColor.WARPED_NYLIUM).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "oxidized_rail")))), "oxidized_copper_rail");
+    public static final Block WAXED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.COPPER_RAIL).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "waxed_copper_rail")))), "waxed_copper_rail");
+    public static final Block WAXED_EXPOSED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.EXPOSED_COPPER_RAIL).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "waxed_exposed_copper_rail")))), "waxed_exposed_copper_rail");
+    public static final Block WAXED_WEATHERED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.WEATHERED_COPPER_RAIL).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "waxed_weathered_copper_rail")))), "waxed_weathered_copper_rail");
+    public static final Block WAXED_OXIDIZED_COPPER_RAIL = register(new GenericCopperRailBlock(BlockBehaviour.Properties.ofFullCopy(ModBlocks.OXIDIZED_COPPER_RAIL).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "waxed_oxidized_copper_rail")))), "waxed_oxidized_copper_rail");
+    public static final Block RAIL_CROSSING = register(new CrossingRailBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.POWERED_RAIL).setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, "rail_crossing")))), "rail_crossing");
 
     public static void initialize() {
         putInRedstoneTab(ModBlocks.COPPER_RAIL);
@@ -58,7 +62,7 @@ public class ModBlocks {
 
     private static Block register(Block block, String id) {
         ResourceLocation blockID = ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, id);
-        BlockItem blockItem = new BlockItem(block, new Item.Properties());
+        BlockItem blockItem = new BlockItem(block, new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(CopperRails.MOD_ID, id))));
         Registry.register(BuiltInRegistries.ITEM, blockID, blockItem);
         return Registry.register(BuiltInRegistries.BLOCK, blockID, block);
     }
